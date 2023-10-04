9fadea5c4a419dda10958495c61ac10bb89cf961
ramya <ramya.guptha@tisys.co.in>
2023-10-04 18:46:05 +0530

    [ADD] Incident_management module
    Incident management module is used for reporting incidents, investigation, finding root cause and taking corrective actions. Skeleton for this project is committed.

---
+++ custom_addons/incident_management/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Incident Management',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
