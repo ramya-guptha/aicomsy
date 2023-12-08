3d5abaadd30941db294ad60585c70f30be6552af
ramya <ramya.guptha@tisys.co.in>
2023-12-08 14:28:16 +0530

    [ADD] Non Conformance - Settings for master configurations
    Modified menu and manifest file,Added report_lov_views

--- custom_addons/non_conformance_report/__manifest__.py
+++ custom_addons/non_conformance_report/__manifest__.py
@@ -9,6 +9,7 @@
         'data/ir_sequence_data.xml',
         'views/x_ncr_report_views.xml',
         'views/x_ncr_nc_views.xml',
+        'views/x_ncr_report_lov_views.xml',
         'views/x_ncr_menu.xml',
         'views/x_ncr_response_views.xml',
         'data/x_ncr_type_data.xml',
